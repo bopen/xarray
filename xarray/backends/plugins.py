62ce625e1b423cb8cab472094be4d709f4b5feeb
Aureliana Barghini <a.barghini@bopen.eu>
2020-11-20 12:04:42 +0100

    function rename in plugins

--- xarray/backends/plugins.py
+++ xarray/backends/plugins.py
@@ -4,8 +4,7 @@ import typing as T
 import warnings
 from functools import lru_cache
 
-import pkg_resources  # type: ignore
-
+import pkg_resources
 
 class BackendEntrypoint:
     __slots__ = ("open_dataset", "open_dataset_parameters")
@@ -15,7 +14,7 @@ class BackendEntrypoint:
         self.open_dataset_parameters = open_dataset_parameters
 
 
-def create_entrypoints_dict(backend_entrypoints):
+def remove_duplicates(backend_entrypoints):
 
     # sort and group entrypoints by name
     key_name = lambda ep: ep.name
@@ -24,19 +23,19 @@ def create_entrypoints_dict(backend_entrypoints):
         backend_entrypoints, key=key_name
     )
     # check if there are multiple entrypoints for the same name
-    entrypoint_dict = {}
+    unique_backend_entrypoints = []
     for name, matches in backend_entrypoints_grouped:
         matches = list(matches)
+        unique_backend_entrypoints.append([name, matches[0]])
         matches_len = len(matches)
-        entrypoint_dict[name] = matches[0]
         if matches_len > 1:
-            selected_module_name = entrypoint_dict[name].module_name
+            selected_module_name = matches[0].module_name
             all_module_names = [e.module_name for e in matches]
             warnings.warn(
                 f"\nFound {matches_len} entrypoints for the engine name {name}:"
                 f"\n {all_module_names}.\n It will be used: {selected_module_name}.",
             )
-    return entrypoint_dict
+    return unique_backend_entrypoints
 
 
 def detect_parameters(open_dataset):
@@ -56,13 +55,13 @@ def detect_parameters(open_dataset):
 
 def load_entrypoints(backend_entrypoints):
     engines: T.Dict[str, T.Dict[str, T.Any]] = {}
-    for name, backend in backend_entrypoints.items():
+    for name, backend in backend_entrypoints:
         backend = backend.load()
         engines[name] = backend
     return engines
 
 
-def set_parameters(engines):
+def set_missing_parameters(engines):
     for name, backend in engines.items():
         if backend.open_dataset_parameters is None:
             open_dataset = backend.open_dataset
@@ -71,8 +70,8 @@ def set_parameters(engines):
 
 @lru_cache(maxsize=1)
 def detect_engines():
-    backend_entrypoints = pkg_resources.iter_entry_points("xarray.backends")
-    backend_entrypoints = create_entrypoints_dict(backend_entrypoints)
+    entrypoints = pkg_resources.iter_entry_points("xarray.backends")
+    backend_entrypoints = remove_duplicates(entrypoints)
     engines = load_entrypoints(backend_entrypoints)
-    set_parameters(engines)
+    set_missing_parameters(engines)
     return engines
