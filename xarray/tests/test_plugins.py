6224402bc7edb321d220d8a42b50418f4cf17da9
Aureliana Barghini <a.barghini@bopen.eu>
2020-11-20 18:29:33 +0100

    style: change names to standardize code

--- xarray/tests/test_plugins.py
+++ xarray/tests/test_plugins.py
@@ -7,8 +7,8 @@ dummy_open_dataset_args = lambda filename_or_obj, *args: None
 dummy_open_dataset_kwargs = lambda filename_or_obj, **kwargs: None
 dummy_open_dataset = lambda filename_or_obj, *, decoder: None
 
-backend_ep_1 = plugins.BackendEntrypoint(dummy_open_dataset)
-backend_ep_2 = plugins.BackendEntrypoint(dummy_open_dataset)
+backend_1 = plugins.BackendEntrypoint(dummy_open_dataset)
+backend_2 = plugins.BackendEntrypoint(dummy_open_dataset)
 
 
 def instantiate_entrypoints(specs):
@@ -22,10 +22,10 @@ def instantiate_entrypoints(specs):
 @pytest.fixture
 def dummy_duplicated_entrypoints():
     spec = [
-        "engine1 = xarray.tests.test_plugins:backend_ep_1",
-        "engine1 = xarray.tests.test_plugins:backend_ep_2",
-        "engine2 = xarray.tests.test_plugins:backend_ep_1",
-        "engine2 = xarray.tests.test_plugins:backend_ep_2",
+        "engine1 = xarray.tests.test_plugins:backend_1",
+        "engine1 = xarray.tests.test_plugins:backend_2",
+        "engine2 = xarray.tests.test_plugins:backend_1",
+        "engine2 = xarray.tests.test_plugins:backend_2",
     ]
     dummy_duplicated_entrypoints = instantiate_entrypoints(spec)
     return dummy_duplicated_entrypoints
@@ -50,8 +50,8 @@ def test_remove_duplicates_wargnings(dummy_duplicated_entrypoints):
 
 def test_create_engines_dict():
     spec = [
-        "engine1 = xarray.tests.test_plugins:backend_ep_1",
-        "engine2 = xarray.tests.test_plugins:backend_ep_2",
+        "engine1 = xarray.tests.test_plugins:backend_1",
+        "engine2 = xarray.tests.test_plugins:backend_2",
     ]
     entrypoints = instantiate_entrypoints(spec)
     engines = plugins.create_engines_dict(entrypoints)
